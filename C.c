b35b1fe3c858804e7f21186c3fb727d83ddc2137
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-07-12 09:55:23 +0530

    First commit

---
+++ C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int tt;
+  scanf("%d",&t);
+  for(int ii=0;ii<tt;ii++) {
+
+
+    printf("Case #%d: %d\n",(ii+1),INSERT);
+  }
+}
---
+++ D.py
@@ -0,0 +1,4 @@
+nn = int(input())
+for ii in range(nn):
+
+    print('Case #'+str(ii+1)+': '+str(INSERT))
